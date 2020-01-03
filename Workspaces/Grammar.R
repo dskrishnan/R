d1e4bbdb9660f2058880e9269017a7394bb29c4f
dskrishnan <16497990+dskrishnan@users.noreply.github.com>
2020-01-03 11:16:38 +0530

    Reframed the material as Grammar & Exercises

--- Workspaces/Grammar.R
+++ Workspaces/Grammar.R
@@ -1,4 +1,4 @@
-# ********************* This file "R - eDX.R" is an R script.*******************************
+# ********************* This file "Grammar.R" is an R script.*******************************
 # This is a comment.
 
 "
@@ -59,28 +59,21 @@ rm(list=ls()) # This will remove all the previously declared/assigned objects(ak
 clr <- function() cat("\014") # Cat -> Concatenate and Print. Here '<-' is prefered as assignment operator. 
 # The above can be invoked as
 clr()
-# Both '<-' & '=' can be used as assignment operator. However inside functions, only '=' is permitted as assignment operator.
+# 
 
+# Assignment Operators:
 "
-   In any programming language variables are nothing but reserved memory locations to store values.
-   This means when a variable is created, some space is reserved in memory. We may like to store
-   information of various data types such as like character, wide character, integer, floating point,
-   double floating point, Boolean etc. Based on the data type of a variable, the operating system 
-   allocates memory and decides what can be stored in the reserved memory.
-
-   In contrast to other programming languages like C and java, variables are not declared 
-   as some data type in R. The variables are called as R-Objects. The datatype of R Object is called as Class. 
-   There are many types/class of R-objects. The frequently used
-   used ones are,
-      Vectors
-      Lists
-      Matrices
-      Arrays
-      Factors
-      Data Frames
-The Simplest of these objects is the Vector Object and there are six types of these atomic vectors,
-also termed as six classes of vectors. The other R-Objects are built upon the atomic vectors
+      <-, <<-, = : Leftward Assignment
+      ->, ->> : Rightward Assignment.
+   Both '<-' & '=' can be used almost interchangeably except inside functions.
+   Only '=' is permitted as assignment operator inside functions.
+   
+   The '<<-' operator is used for assigning to variables, in the parent environments(more like global assignments).
+   The rightward assignments, although available are rarely used.
 "
+x <- 5 # Leftward Assignment
+x = 9  # This is also left Assignment
+10 -> x # Rightward Assignment. Same as x <- 10 or x = 10.
 
 # Rules for declarating Varibles/Objects
 "
@@ -114,24 +107,63 @@ Constants in R
       
    Character Constants:
       They are represented using either single quotes '' or double qoutes "" as delimiters.
-      #Ex. 'example' -> typeof('example') would be "character".
+      #Ex. 'example' -> typeof('example') would be character.
    
    Built-in Constants:
       Some of the built in Constants are
-         #LETTERS: This means "A" "B" "C" "D" "E" "F"......X" "Y" "Z"
-         #letters: This means "a" "b" "c" "d" "e"...."x" "y" "z"
+         #LETTERS: This means 'A' 'B' 'C' D' 'E' 'F'......'X' 'Y' 'Z'
+         #letters: This means 'a' 'b' 'c' 'd' 'e'....'x' 'y' 'z'
          #pi : This means 3.141593
-         #month.name: This means "January"   "February"  "March"..."October"   "November"  "December"
-         #month.abb: This means "Jan" "Feb" "Mar"..."Oct" "Nov" "Dec"
+         #month.name: This means 'January'   'February'  'March'...'October' 'November' 'December'
+         #month.abb: This means 'Jan' 'Feb' 'Mar'...'Oct' 'Nov' 'Dec'
       But it is not good do rely on these built in constants, because these values can be changed
       For example pi <- 56 would force the value of pi from 3.141593 to 56.
-"
 
+"
 # R Operators
 
+x <- 5
+y <- 16
 
+# Following are Arithmetic Operators.
+addition <- x+y
+subtraction <- x-y
+multiplication <- x*y
+division <- y/x
+modulas.remainder.from.division <- y%%x
+integer.division <- y%/%x
+exponent <- y^x
 
+# Following are Relational Operators,
+x < y # Less Than Operator. Ans is TRUE
+x > y # Greater Than Operator. Ans is 
+x <= 5 # Less Than or Equal to. Ans is
+y >= 20 # Greater Than or Equal to. Ans is
+y == 16 # Equal to
+x != 5 # Not Equal to
 
+# Operation on vectors
+"
+   In any programming language variables are nothing but reserved memory locations to store values.
+   This means when a variable is created, some space is reserved in memory. We may like to store
+   information of various data types such as like character, wide character, integer, floating point,
+   double floating point, Boolean etc. Based on the data type of a variable, the operating system 
+   allocates memory and decides what can be stored in the reserved memory.
+
+   In contrast to other programming languages like C and java, variables are not declared 
+   as some data type in R. The variables are called as R-Objects. The datatype of R Object is called as Class. 
+   There are many types/class of R-objects. The frequently used
+   used ones are,
+      Vectors
+      Lists
+      Matrices
+      Arrays
+      Factors
+      Data Frames
+      
+The Simplest of these objects is the Vector Object and there are six types of these atomic vectors,
+also termed as six classes of vectors. The other R-Objects are built upon the atomic vectors
+"
 # Logical Vector(Object/Class/Datatype), Ex: TRUE, FALSE
 aLogicalObject <- FALSE
 class(aLogicalObject)
@@ -167,21 +199,18 @@ is.raw(aStringObject) # Answer is FALSE
  In R Programming, the basic data types are the R objects called vectors which hold the elements
  of different classes. In R the number of classes is not confined only to the above types.
  For example we can use many atomic vectors and create an array whose class will become array.
-"
+
+ A Vector is what is called an array in all other programming languages except R. Vector is 
+ a basic data structure in R. It contains elements of same type. The data types can be logical,
+ Integer, double, character, complex or raw. Vector's type can be checked using typeof() function.
+
+ To create a vector with more than one element, the function c() should be used. The default
+ method combines its arguments to form a vector. All Arguments are coerced to a common type
+ which is the type of the returned value, and all attributes except names are removed.
 
 "
-A Vector is what is called an array in all other programming languages except R. Vector is 
-a basic data structure in R. It contains elements of same type. The data types can be logical,
-Integer, double, character, complex or raw. Vector's type can be checked using typeof() function.
-
-To create a vector with more than one element, the function c() should be used. The default
-method combines its arguments to form a vector. All Arguments are coerced to a common type
-which is the type of the returned value, and all attributes except names are removed.
-
-"
-
 color1 <- c("red", "green", "yellow") # This is a vector with 3 elements
-color1 # It does print(color1)
+color1 # It amounts to `      'print(color1)'
 class(color1) # Class of this vector would be Character.
 oddNumbersVectorArray <- c(1,3,5,7,9)
 class(oddNumbersVectorArray)# Class of this vector would be numeric.
@@ -189,7 +218,6 @@ mixedVectorArray <- c(2,4,6,8,"ABCD")
 class(mixedVectorArray) #Class of this vector would be Character.
 
 multiVector <- c(aRawObject, aStringObject, aComplexObject, anIntegerObject, aNumericObject, aLogicalObject)
-
 "
  Since, a vector must have elements of same type, this function c() will try and coerce 
  elments to same type, if they're different.
@@ -197,13 +225,83 @@ multiVector <- c(aRawObject, aStringObject, aComplexObject, anIntegerObject, aNu
  Coercion is from lower to higher types from logical to integer to double to character
    raw < logical < integer < double < complex < character < list
 "
-#Thus
+# Creating Vector using ':' Operator
+x <- 1:7 # This will be int[1:7] 1 2 3 4 5 6 7
+y <- 2:-2 # This will be int[1:5] 2 1 0 -1 -2
 
+# Creating Vector using seq() Operator
+x <- seq(1,3, by=0.5) #Specify Step Size. This will be num[1:5] 1.0 1.5 2.0 2.5 3.0
+y <- seq(1,5, length.out = 4) # This will num be 'num[1:4] 1 2.33 3.67 5'. It creates 4 elements which are equally spaced out from one another
+# y <- seq(1,5, length.out = 4) & y <- seq(1,5, length = 4) & y <- seq(1,5, len = 4) are all same. This is called partial Matching.
+# Refer https://stackoverflow.com/questions/14153904/partial-matching-of-function-argument
 
+# Accessing a Vector
+
+x <- seq(0,10, by=2) # This will be num[1:6] 0 2 4 6 8 10
+
+# Vector Indexing starts from 1 in R unlike other programming languages where index starts from 0.
+x[3] # Access 3rd element '4'
+
+# We can use vector of integers as index to access specific elements.
+x[c(2, 4)] # Access 2nd and 4th element. This will be '2 6'
+
+# We can also use negative integers to return all elements except that those specified.
+x[-1] # Access all but 1st element. This will be '2 4 6 8 10'
+
+#But we cannot mix positive and negative integers while indexing
+x[c(2, -4)] # Cannot mix positive & negative integers. This will be 'Error in x[c(2, -4)] : only 0's may be mixed with negative subscripts'
+
+# Real numbers should not be used as index. If used, it will be truncated(not rounded off) to integers.
+x[c(2.45,4.54)] # Real numbers  are trucated(not rounded off) to integers. This will be '2 6'
+
+# Logical Vector as Index
+x[c(TRUE, FALSE, FALSE, TRUE, FALSE, TRUE)] # The positions where Logical VECTOR is TRUE will be returned. This will be '0 6 10'
+
+x [x < 5] #Filtering Vector based on condition. This will be '0 2 4'
+x < 5 # This will be logical vectors 'TRUE  TRUE  TRUE FALSE FALSE FALSE'
+
+# Using Character Vector as Index
+x <- c("First" = 1, "Second" = 2, "Third"=3) 
+names(x) # This will be ' "First"  "Second" "Third" '
+x["Second"] # This will be '2'
+x["First","Third"] # This is wrong. Incorrect number of dimensions.
+x[c("First","Third")] # This will be '1, 3'
+
+# Modify Vector
+
+x <- -3:3 # This vector will be '-3 -2 -1  0  1  2  3'
+x[2] <- 0 # Modify 2nd Element. This will be '-3  0 -1  0  1  2  3'
+x[x < 0] <- 5 # Modify elements less than 0. This will be '5 0 5 0 1 2 3'
+x[1:4] # Truncate x to first 4 elements. This will be '5 0 5 0' (But x <- 1:4 This will create 1 2 3 4 )
+
+# Deleting a Vector
+x <- NULL
+print(x) # THis will be NULL.
+x[4] # This will be NULL.
+
+# Add two Vectors
+x <- c(3,6,8)
+y <- c(2,9,0)
+z <- x + y # This will be '5 15 8'
+x1 <- x + 1 # This will recycle 1 or add 1 to every element of x
+x12 <- x + c(1,2) # '1,2' are recycled through '3,6,8' but a warning is issued. This will be '4,6,8'
+
+# Logical Operators
+x <- c(TRUE,FALSE,0,6)
+LogicalNot.x <- !x # This will be 'FALSE  TRUE  TRUE FALSE'. Zero is considered as FALSE & Non Zero Numbers are considered as TRUE.
+
+y <- c(FALSE,TRUE,FALSE,TRUE)
+xElemntANDy <- x&y # This is element wise Logical AND producing length of the longer operand . Result will be 'FALSE FALSE FALSE TRUE'
+xLogicalANDy <- x&&y # This is logical AND. This Examines only the first element of the Operands resulting into Single length Logical Vector. Result will be 'FALSE'.
+xElementORy <- x|y #This is element wise Logical OR producing result having length of the longer operand. This will be 'TRUE  TRUE FALSE  TRUE'
+xLogicalORy <- x||y # This is Logical OR. This Examines only the first element of the Operands resulting into Single length Logical Vector. Result will be 'TRUE'.
+
+# R Flow Control
 
 "
-Lists: A list is an R Object which can contain many types of elements inside it like vectors, functions and even another list.
-List Size can be increased on the fly. List contents can be accessed either by index or by name
+   Lists: A list is an R Object which can contain many types of elements inside it like vectors, 
+   functions and even another list. List Size can be increased on the fly. List contents can be 
+   accessed either by index or by name
 "
 list1 <- list(c(1,2,3), 35.8, sin,100) # Creating a list.
 list2 <- list(oddVector=c(9,7,5,3,1),func=sin, string="Hello World")
